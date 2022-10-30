4d07c01deadde97498ec01cbe6d335c05018c0cf
Nichlas Schipper <Nichlas12323@gmail.com>
2022-10-30 02:08:06 +0100

    initial project

---
+++ src/main/java/net/nicschi/moremobility/MoreMobility.java
@@ -0,0 +1,15 @@
+package net.nicschi.moremobility;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreMobility implements ModInitializer {
+	public static final String MOD_ID = "more-mobility";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nicschi/moremobility/MoreMobilityClient.java
@@ -0,0 +1,12 @@
+package net.nicschi.moremobility;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreMobilityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
